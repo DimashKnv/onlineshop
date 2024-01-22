11e23b15cc88dd339f092dfa9148271f6fcc89ff
Dimash Knv <kunakpayevd@gmail.com>
2024-01-22 22:23:42 +0600

    cart_page_payment_upd

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,52 @@
 import { useSelector } from "react-redux";
 import { CartCard } from "./CartCard";
-export function Cart(){
-const cartState=useSelector((state)=>state.cartReducer.cart);
-    
-    return <div>
-        {cartState.map((object)=>{return <CartCard info={object}></CartCard>})}
+export function Cart() {
+  const cartState = useSelector((state) => state.cartReducer.cart);
+
+  return (
+    <div className="cart_page">
+      <h1>Корзина товаров</h1>
+      <div className="cart_page_main">
+        <div className="cart_table">
+          <div className="cart_table_row">
+            <div className="cart_table_cell">
+              <p>Наименование</p>
+            </div>
+            <div className="cart_table_cell">
+              <p>Количество</p>
+            </div>
+            <div className="cart_table_cell">
+              <p>Цена</p>
+            </div>
+            <div className="cart_table_cell">
+              <p>Стоимость</p>
+
+            </div>
+            <div className="cart_table_cell">
+                
+            </div>
+          </div>
+          {cartState.map((object) => {
+            return <CartCard info={object}></CartCard>;
+          })}
+          <div className="cart_table_bot">
+            <button>Продолжить покупки</button>
+            <h2>Итого:</h2>
+          </div>
+        </div>
+        <div className="cart_payment">
+          <label htmlFor="">Имя владельца карты</label>
+          <input type="text" />
+          <label htmlFor="">Номер карты</label>
+          <input type="number" />
+          <label htmlFor="">Срок действия карты</label>
+          <input type="number" />
+          <input type="number" />
+          <label htmlFor="">CVV</label>
+          <input type="number" />
+          <button>Подтвердить оплату</button>
+        </div>
+      </div>
     </div>
+  );
 }
--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,5 +1,24 @@
+import Xpng from "../assets/Vector.png";
 
+export function CartCard(props) {
+  console.log(props.info);
+  return (
+    <div className="cart_table_row">
+      <div className="cart_table_cell">
+        <img className="cart_card_img" src={props.info.image} alt="" />
+        <p>{props.info.title}</p>
+      </div>
+      <div className="cart_table_cell">
+        <p>0shtuk</p>
+      </div>
+      <div className="cart_table_cell">
+        <p>{props.info.price}</p>
+      </div>
+      <div className="cart_table_cell">
+        <p>stoimost'</p>
+      </div>
 
-export function CartCard(props){
-    return <h1>{props.info.title}</h1>
+      <img src={Xpng} alt="" />
+    </div>
+  );
 }
